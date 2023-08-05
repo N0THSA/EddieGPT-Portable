67eada12955d0558304d36e9c912968bacd16ec7
Ash <76790588+N0THSA@users.noreply.github.com>
2023-08-05 13:52:22 -0400

    Update quickstart.py
    update name from EddieGPT Portable to LLM Model TUI (for low end pcs) and add custom model selection

--- quickstart.py
+++ quickstart.py
@@ -5,13 +5,19 @@ from colorama import Fore as col
 print("WARNING: YOU NEED INTERNET ACCESS THE FIRST TIME RUNNING ON THIS DEVICE.")
 print("Internet access needed when plugged into new device.")
 sleep(5)
+print("\n================================")
+print("LLM Model TUI (for low end pcs)")
+print("================================\n")
 print("MODEL SELECTION:")
 print(f"{col.YELLOW}[{col.RESET}1{col.YELLOW}]{col.RESET} orca-mini-3b.ggmlv3.q4_0.bin (Small, fast and dumb. 4GB download.)")
 print(f"{col.YELLOW}[{col.RESET}2{col.YELLOW}]{col.RESET} wizardLM-13B-Uncensored.ggmlv3.q4_0.bin (Medium sized, slow and smart. Uncensored model. 8GB download.)")
 print(f"{col.YELLOW}[{col.RESET}3{col.YELLOW}]{col.RESET} nous-hermes-13b.ggmlv3.q4_0.bin (Medium sized, slow and smartest. Nearing text-davinci-003 capabilities. 8GB download.)")
 print(f"{col.YELLOW}[{col.RESET}4{col.YELLOW}]{col.RESET} wizardlm-13b-v1.1-superhot-8k.ggmlv3.q4_0.bin (Best overall model. Medium sized, slow and smart. Gives very long responses. 8GB download.")
+print(f"\n{col.YELLOW}[{col.RESET}C{col.YELLOW}]{col.RESET} Custom model from NomicAI (at gpt4all.io)")
 selection = input("> ")
-if selection == "1":
+if selection == "c":
+  model = input("NomicAI model? (gpt4all.io): ")
+elif selection == "1":
   model = "orca-mini-3b.ggmlv3.q4_0.bin"
 elif selection == "2":
   model = "wizardLM-13B-Uncensored.ggmlv3.q4_0.bin"
@@ -26,7 +32,7 @@ else:
 print(f"\nModel chosen: {model}")
 sleep(2)
 print("Minimum Specifications:\n\n12GB RAM\nNo GPU\nQuad-Core Ryzen 5 / Core I5")
-print("It is not Null Industries fault if you experience issues under the minimum specifications.")
+print("It is not my fault if you experience issues under the minimum specifications.")
 sleep(10)
 print("\nModel downloading / loading will begin in a moment...")
 sleep(2.352)
@@ -42,5 +48,5 @@ def chatloop():
 
 print(f"{col.LIGHTGREEN_EX}MODEL LOADED\n")
 sleep(2)
-print(f"{col.RESET}Due to limitations, Porta-Eddie will repeat himself if given\nthe same prompt twice in a row. Also, Porta-Eddie does not have the\ncapability to remember anything.\n\n")
+print(f"{col.RESET}Due to limitations, the chatbot will repeat himself if given\nthe same prompt twice in a row. Also, the chatbot does not have the\ncapability to remember anything.\n\n")
 chatloop()
